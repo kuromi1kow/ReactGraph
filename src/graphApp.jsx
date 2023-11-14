899f23b26ca7ac0af7f1457678134bcf2956db75
Assylkhan kuromi1kow <112749419+kuromi1kow@users.noreply.github.com>
2023-11-14 10:09:15 +0600

    GraphWhichWork

--- src/graphApp.jsx
+++ src/graphApp.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import Papa from 'papaparse';
 import ReactECharts from 'echarts-for-react';
+import 'echarts';
+
 
 const Chart = () => {
   const chartRef = useRef(null);
@@ -60,7 +62,7 @@ const Chart = () => {
       const rawData = [
         ['2010-01-01', 100, 110, 95, 105, 150000],
   ['2010-02-01', 105, 115, 98, 108, 180000],
-  ['2010-03-01', 108, 120, 100, 112, 200000],
+  ['2010-03-01', 108, 120, 100, 112, 100000],
   ['2010-04-01', 112, 125, 105, 116, 220000],
   ['2010-05-01', 116, 130, 108, 120, 240000],
   ['2010-06-01', 120, 135, 112, 125, 260000],
@@ -69,7 +71,7 @@ const Chart = () => {
   ['2010-09-01', 135, 150, 125, 140, 320000],
   ['2010-10-01', 140, 155, 130, 145, 340000],
   ['2010-11-01', 145, 160, 135, 150, 360000],
-  ['2010-12-01', 150, 165, 140, 155, 380000],
+  ['2010-12-01', 100, 120, 120, 125, 180000],
   // Continue the pattern for subsequent years
   ['2016-01-01', 180, 200, 170, 190, 450000],
   ['2016-02-01', 185, 205, 175, 195, 470000],
@@ -90,7 +92,7 @@ const Chart = () => {
         legend: {
           bottom: 10,
           left: 'center',
-          data: ['OPEN', 'HIGH', 'LOW', 'TURNOVER', 'VOLATILITY'],
+          data: ['Assylkhans index', 'First', 'Second', 'Third', '4th', 'Volume']
         },
         tooltip: {
           trigger: 'axis',
@@ -170,7 +172,7 @@ const Chart = () => {
           {
             type: 'category',
             data: data.categoryData,
-            boundaryGap: false,
+            boundaryGap: true, // Set boundaryGap to false
             axisLine: { onZero: false },
             splitLine: { show: false },
             min: 'dataMin',
@@ -183,7 +185,7 @@ const Chart = () => {
             type: 'category',
             gridIndex: 1,
             data: data.categoryData,
-            boundaryGap: false,
+            boundaryGap: true,
             axisLine: { onZero: false },
             axisTick: { show: false },
             splitLine: { show: false },
@@ -199,18 +201,14 @@ const Chart = () => {
               show: true
             }
           },
-         {
+          {
+            scale: true,
             gridIndex: 1,
             splitNumber: 2,
-            axisLabel: {
-              show: true,
-              formatter: function (value) {
-                return value / 1000000 + 'M';
-              }
-            },
+            axisLabel: { show: false },
             axisLine: { show: false },
             axisTick: { show: false },
-            splitLine: { show: false },
+            splitLine: { show: false }
           }
         ],
         dataZoom: [
@@ -231,94 +229,67 @@ const Chart = () => {
         ],
         series: [
           {
-            name: 'OPEN',
-            type: 'line',
-            data: data.values.map((item) => item[1]),
-            smooth: true,
-            lineStyle: {
-              opacity: 0.5
-            },
-          },
-          {
-            name: 'HIGH',
-            type: 'line',
-            data: data.values.map((item) => item[2]),
-            smooth: true,
-            lineStyle: {
-              opacity: 0.5
-            },
-          },
-          {
-            name: 'LOW',
-            type: 'line',
-            data: data.values.map((item) => item[3]),
-            smooth: true,
-            lineStyle: {
-              opacity: 0.5
-            },
-          },
-          {
-            name: 'TURNOVER',
+            name: 'Assylkhan index',
             type: 'candlestick',
-            xAxisIndex: 1,
-            yAxisIndex: 1,
-            data: [
-              [135, 150, 125, 140, 320000],
-              [140, 155, 130, 145, 340000],
-              [145, 160, 135, 150, 360000],
-              [150, 165, 140, 155, 380000],
-              [180, 200, 170, 190, 450000],
-              [185, 205, 175, 195, 470000],
-              [190, 210, 180, 200, 490000],
-              [195, 215, 185, 205, 510000],
-              [200, 220, 190, 210, 530000],
-              [205, 225, 195, 215, 550000],
-              [210, 230, 200, 220, 600000],
-              [215, 235, 205, 225, 620000],
-              [220, 240, 210, 230, 650000],
-              [225, 245, 215, 235, 680000],
-              [230, 250, 220, 240, 700000],
-              [235, 255, 225, 245, 720000],
-              [240, 260, 230, 250, 750000],
-              [245, 265, 235, 255, 780000],
-              [250, 270, 240, 260, 800000],
-              [255, 275, 245, 265, 820000],
-              [135, 150, 125, 140, 320000],
-              [140, 155, 130, 145, 340000],
-              [145, 160, 135, 150, 360000],
-              [190, 165, 140, 155, 380000],
-              [1980, 200, 170, 190, 450000],
-              [985, 205, 175, 195, 470000],
-              [990, 210, 180, 200, 490000],
-              [995, 215, 185, 205, 510000],
-              [900, 220, 190, 210, 530000],
-              [905, 225, 195, 215, 550000],
-              [910, 230, 200, 220, 600000],
-              [915, 235, 205, 225, 620000],
-              [920, 240, 210, 230, 650000],
-              [925, 245, 215, 235, 680000],
-              [930, 250, 220, 240, 700000],
-              [935, 255, 225, 245, 720000],
-              [940, 260, 230, 250, 750000],
-              [945, 265, 235, 255, 780000],
-              [850, 270, 240, 260, 800000],
-              [280, 275, 245, 265, 820000],
-            ],
+            data: data.values,
             itemStyle: {
-              color: upColor,   // Цвет свечи при положительном изменении
-              color0: downColor // Цвет свечи при отрицательном изменении
-            }
+              color: upColor,
+              color0: downColor,
+              borderColor: undefined,
+              borderColor0: undefined
+            },
+            emphasis: {
+              itemStyle: {
+                color: upColor, // Set the color for emphasis state if needed
+                color0: downColor,
+                borderColor: undefined,
+                borderColor0: undefined
+              },
+            },
           },
           {
-            name: 'VOLATILITY',
+            name: 'first',
             type: 'line',
-            xAxisIndex: 1,
-            yAxisIndex: 1,
             data: calculateMA(5, data),
             smooth: true,
             lineStyle: {
               opacity: 0.5
-            },
+            }
+          },
+          {
+            name: 'second',
+            type: 'line',
+            data: calculateMA(10, data),
+            smooth: true,
+            lineStyle: {
+              opacity: 0.5
+            }
+          },
+          {
+            name: 'third',
+            type: 'line',
+            data: calculateMA(20, data),
+            smooth: true,
+            lineStyle: {
+              opacity: 0.5
+            }
+          },
+          {
+            name: '4th',
+            type: 'line',
+            data: calculateMA(30, data),
+            smooth: true,
+            lineStyle: {
+              opacity: 0.5
+            }
+          },
+          {
+            
+            name: 'Volume',
+            type: 'bar',
+            xAxisIndex: 1,
+            yAxisIndex: 1,
+            data: data.volumes
           }
         ]
       });
@@ -334,9 +305,9 @@ const Chart = () => {
   };
 
   return (
-    <div style={{ width: 'auto', height: 'auto' }}>
-      <ReactECharts ref={chartRef} option={getOption()} />
-    </div>
+    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
+    <ReactECharts ref={chartRef} option={getOption()} style={{ width: '100%', height: '100%' }} />
+  </div>
   );
 };
 
